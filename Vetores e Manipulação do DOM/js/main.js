b6131de10d0b237505551bdc81ec9da7f4cee783
Luiz <luiz.felipe.florian.silva12@gmail.com>
2025-04-17 22:50:13 -0300

    pratica de vetores usando localStorage

--- Vetores e Manipulação do DOM/js/main.js
+++ Vetores e Manipulação do DOM/js/main.js
@@ -1,13 +1,41 @@
 let inputItem = document.querySelector('#item');
 let btnAdicionar = document.querySelector('#btnAdicionar');
 let ulListaCompras = document.querySelector('#listaCompras');
+let contador = document.querySelector('#contador');
+
+let listaCompras = [];
+
+if (localStorage.getItem('lista')) {
+    listaCompras = JSON.parse(localStorage.getItem('lista'));
+}
 
 function adicionarItem() {
     let item = inputItem.value;
-    let li = document.createElement("li");
-    li.textContent = item;
-    li.classList.add('list-item', 'border-b-1', 'border-primary', 'p-2');
-    ulListaCompras.appendChild(li);
+    if (item.trim() === '') { // Verifica se o campo está vazio ou contém apenas espaços
+        alert('Por favor, insira um item!');
+        return;
+    }
+    listaCompras.push(item);
+    renderLista();
+    atualizarContador();
+    localStorage.setItem('lista', JSON.stringify(listaCompras));
 }
 
-btnAdicionar.onclick = adicionarItem
+function renderLista() {
+    ulListaCompras.innerHTML = ''; // Limpa a lista antes de renderizar novamente
+    for (let item of listaCompras) {
+        let li = document.createElement('li');
+        li.textContent = item;
+        li.classList.add('list-item', 'border-b-1', 'border-primary', 'p-2');
+        ulListaCompras.appendChild(li);
+        console.log(listaCompras);
+    }
+}
+
+function atualizarContador() {
+    contador.textContent = "Total de itens: " + listaCompras.length;
+}
+renderLista();
+atualizarContador();
+
+btnAdicionar.onclick = adicionarItem;
