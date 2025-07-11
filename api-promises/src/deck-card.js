3c6a3a097e0e3cd9bfed7ce75d5f82887fda5efb
luizfelipeflorian <luiz.felipe.florian.silva12@gmail.com>
2025-07-10 23:48:00 -0300

    feat: implementar lógica para criação e manipulação de baralho no jogo de cartas

--- api-promises/src/deck-card.js
+++ api-promises/src/deck-card.js
@@ -1,10 +1,45 @@
 import './style.css'
 
+//tutorial para instalar os icones do lucide
+//https://lucide.dev/guide/packages/lucide
 import { Coins, createIcons, Dices, Layers } from 'lucide'
+import { comprarCarta, criarBaralho } from "./api/deck-of-cards";
 
 createIcons({
     icons: {
         Dices,
         Layers, Coins
     }
+})
+
+const btnNovoBaralho = document.querySelector('#btnNovoBaralho');
+const btnResetarMesa = document.querySelector('#btnResetarMesa');
+const baralhoId = document.querySelector('#baralhoId');
+const cartasRestantes = document.querySelector('#cartasRestantes');
+const mesaBaralho = document.querySelector('#mesaBaralho');
+const mesaCartas = document.querySelector('#mesaCartas');
+
+btnNovoBaralho.addEventListener('click', async function (event) {
+    //fazer um verificador para ver se já existe algum baralho no local storage para dai utilizá-lo ao invés de criar outro//
+
+    const baralho = await criarBaralho();
+    console.log(baralho);
+    localStorage.setItem('baralhoID', baralho.deck_id);
+    const btn = event.target;
+    btn.setAttribute('disabled', true)
+    baralhoId.textContent = baralho.deck_id;
+    cartasRestantes.textContent = baralho.remaining;
+})
+
+mesaBaralho.addEventListener('click', async function () {
+    const cartasCompradas = await comprarCarta(localStorage.getItem('baralhoID'));
+    console.log(cartasCompradas)
+    cartasRestantes.textContent = cartasCompradas.remaining;
+
+})
+
+btnResetarMesa.addEventListener('click', async function () {
+    baralhoId.textContent = "-";
+    cartasRestantes.textContent = '0';
+    btnNovoBaralho.removeAttribute('disabled');
 })
