506cf5dbbadb34af28965996b311a360afba0449
luizfelipeflorian <luiz.felipe.florian.silva12@gmail.com>
2025-09-03 17:49:35 +0000

    finalização exercício 4

--- project-template-vite-main/src/main.js
+++ project-template-vite-main/src/main.js
@@ -47,17 +47,56 @@ console.log(contasArray)
 - Liste as contas **não pagas** e com valor acima de R$ 300. 
 */
 
-let contasPagas = contas.filter((conta)=> {
+let contasPagas = contas.filter((conta) => {
     return (conta.pago)
 })
 console.log(contasPagas)
 
-let contasAcimaDe500 = contas.filter((conta)=> {
+let contasAcimaDe500 = contas.filter((conta) => {
     return (conta.valor > 500)
 })
 console.log(contasAcimaDe500)
 
-let contasNaoPagasAcimaDe500 = contas.filter((conta)=> {
+let contasNaoPagasAcimaDe500 = contas.filter((conta) => {
     return (conta.valor > 500) && (!conta.pago)
 })
 console.log(contasNaoPagasAcimaDe500)
+
+/* 
+4. reduce()
+
+- Calcule o valor total de todas as contas.
+- Calcule o valor total apenas das contas pagas.
+- Calcule o valor total apenas das contas não pagas.
+*/
+
+let total = contas.reduce((ac, despesa) => {
+    return (ac += despesa.valor)
+}, 0)
+console.log(total)
+
+let totalPagas = contas.reduce((ac, despesa) => {
+    return (despesa.pago ? (ac += despesa.valor) : (ac + 0))
+}, 0)
+console.log(totalPagas)
+
+let totalNaoPagas = contas.reduce((ac, despesa) => {
+    return (!despesa.pago ? (ac += despesa.valor) : (ac + 0))
+}, 0)
+console.log(totalNaoPagas)
+
+/* 
+5. some()
+
+- Verifique se existe alguma conta não paga.
+- Verifique se existe alguma conta com valor acima de R$ 1000.
+*/
+
+
+
+/* 
+6. every()
+
+- Verifique se todas as contas já foram pagas.
+- Verifique se todas as contas têm valor acima de R$ 50.
+*/
