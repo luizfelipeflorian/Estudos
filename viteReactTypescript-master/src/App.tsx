84a76d8ac628caf2ffa59e9dfb98d40458738688
luizfelipeflorian <luiz.felipe.florian.silva12@gmail.com>
2025-09-22 21:15:57 -0400

    finalização

--- viteReactTypescript-master/src/App.tsx
+++ viteReactTypescript-master/src/App.tsx
@@ -1,14 +1,23 @@
 import { Button } from "./components/ui/button";
+import { ThemeProvider } from "@/components/theme-provider"
+import { ModeToggle } from "@/components/mode-toggle";
+import { LikeButton } from "./components/like-button";
+
+
 
 export function App() {
   return (
-    <>
+    <><ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
       <div>
         <h1 className="text-3xl font-bold text-center m-4 text-indigo-600">
           Template Projeto Vite -- React/Typescript + TailwindCSS
         </h1>
+        <ModeToggle></ModeToggle>
         <Button>Salvar</Button>
+        <Button variant={"outline"}>Excluir</Button>
+        <LikeButton></LikeButton>
       </div>
+    </ThemeProvider>
     </>
   )
 }
---
+++ viteReactTypescript-master/src/components/like-button.tsx
@@ -0,0 +1,11 @@
+import { Heart } from "lucide-react";
+import { Button } from "./ui/button";
+
+export function LikeButton() {
+    
+    return (
+        <>
+        <Button> <Heart/> Curtir</Button>
+        </>
+    )
+}
