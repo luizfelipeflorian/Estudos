2ce3ea45db5ae1d8da4cce246330f4a2b14d7392
luizfelipeflorian <luiz.felipe.florian.silva12@gmail.com>
2025-09-22 20:30:09 -0400

    primeiro componente botão

--- viteReactTypescript-master/src/App.tsx
+++ viteReactTypescript-master/src/App.tsx
@@ -1,3 +1,5 @@
+import { Button } from "./components/ui/button";
+
 export function App() {
   return (
     <>
@@ -5,6 +7,7 @@ export function App() {
         <h1 className="text-3xl font-bold text-center m-4 text-indigo-600">
           Template Projeto Vite -- React/Typescript + TailwindCSS
         </h1>
+        <Button>Salvar</Button>
       </div>
     </>
   )
