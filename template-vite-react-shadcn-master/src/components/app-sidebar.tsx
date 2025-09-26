e2383e9692928b160cccd8291298bf6148d763ac
luizfelipeflorian <luiz.felipe.florian.silva12@gmail.com>
2025-09-25 20:16:58 -0400

    ajustes

--- template-vite-react-shadcn-master/src/components/app-sidebar.tsx
+++ template-vite-react-shadcn-master/src/components/app-sidebar.tsx
@@ -6,6 +6,7 @@ import {
   BookOpen,
   Bot,
   Command,
+  DiamondPlus,
   Frame,
   GalleryVerticalEnd,
   Map,
@@ -15,7 +16,6 @@ import {
 } from "lucide-react"
 
 import { NavMain } from "@/components/nav-main"
-import { NavProjects } from "@/components/nav-projects"
 import { NavUser } from "@/components/nav-user"
 import { TeamSwitcher } from "@/components/team-switcher"
 import {
@@ -52,21 +52,17 @@ const data = {
   ],
   navMain: [
     {
-      title: "Playground",
+      title: "Cadastro",
       url: "#",
-      icon: SquareTerminal,
+      icon: DiamondPlus,
       isActive: true,
       items: [
         {
-          title: "History",
+          title: "Clientes",
           url: "#",
         },
         {
-          title: "Starred",
-          url: "#",
-        },
-        {
-          title: "Settings",
+          title: "Produtos",
           url: "#",
         },
       ],
@@ -164,7 +160,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
       </SidebarHeader>
       <SidebarContent>
         <NavMain items={data.navMain} />
-        <NavProjects projects={data.projects} />
+
       </SidebarContent>
       <SidebarFooter>
         <NavUser user={data.user} />
